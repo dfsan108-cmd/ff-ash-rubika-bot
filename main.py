946ba091f9546c13f23ac0a8a6486dce06012f5a
dfsan108-cmd <dfsan108@gmail.com>
2026-07-21 21:47:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
 import time
+from flask import Flask
 
-print("FFASHBot Online")
+app = Flask(__name__)
 
-while True:
-    time.sleep(60)
+@app.route("/")
+def home():
+    return "FFASHBot Online"
+
+def run_bot():
+    while True:
+        print("FFASHBot Online")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    from threading import Thread
+
+    Thread(target=run_bot).start()
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
