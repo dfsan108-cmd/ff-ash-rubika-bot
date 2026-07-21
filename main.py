6a10c30db5fd35856293ecc5cbc3e5d5e8d63a14
dfsan108-cmd <dfsan108@gmail.com>
2026-07-21 22:32:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,61 @@ import os
 import time
 from threading import Thread
 from flask import Flask
-import rubika
-print(dir(rubika))
+from rubika import Bot
+
 app = Flask(__name__)
+
+TOKEN = os.environ.get("TOKEN")
+
+if not TOKEN:
+    raise Exception("TOKEN not found")
+
+bot = Bot(TOKEN)
+
 
 @app.route("/")
 def home():
     return "FFASHBot Online"
 
+
 def run_rubika():
-    print("Rubika bot starting...")
-    while True:
-        time.sleep(60)
+    print("FFASHBot Started")
+
+    @bot.on_message()
+    def handler(message):
+        try:
+            text = message.text
+
+            if text == "/start":
+                bot.send_message(
+                    message.chat_id,
+                    "سلام 👋\n🎮 FFASHBot فعال شد"
+                )
+
+            elif text == "سلام":
+                bot.send_message(
+                    message.chat_id,
+                    "سلام 👋\nخوش آمدی"
+                )
+
+            elif text == "تورنومنت":
+                bot.send_message(
+                    message.chat_id,
+                    "🎮 FF ASH Tournament\nثبت‌نام به‌زودی..."
+                )
+
+        except Exception as e:
+            print("ERROR:", e)
+
+    bot.run()
+
 
 if __name__ == "__main__":
     Thread(target=run_rubika).start()
 
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
