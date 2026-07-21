f476e76b01fdc85dbe15f4f8e457ae4cdeac5417
dfsan108-cmd <dfsan108@gmail.com>
2026-07-21 22:23:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import rubika
+import os
 import time
+from threading import Thread
+from flask import Flask
+import rubika
 
-print("Rubika loaded")
+app = Flask(__name__)
 
-print(dir(rubika))
+@app.route("/")
+def home():
+    return "FFASHBot Online"
 
-while True:
-    time.sleep(60)
+def run_rubika():
+    print("Rubika bot starting...")
+    while True:
+        time.sleep(60)
+
+if __name__ == "__main__":
+    Thread(target=run_rubika).start()
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
