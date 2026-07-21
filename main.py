3d3c15751676e65faf52742547d23ac003981a88
dfsan108-cmd <dfsan108@gmail.com>
2026-07-21 21:32:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-while True:
-    print("FFASHBot Online")
+from rubika_bot import Bot
+
+TOKEN = "BJJAIF0JEDWPSZVCWUOSLABHINYJFZWCQVZVEYJJLDRVMXGINJHGUJASVIFEUMUN"
+
+bot = Bot(TOKEN)
+
+@bot.on_message()
+def answer(message):
+    bot.send_message(
+        message.chat_id,
+        "سلام 👋\nFFASHBot روشن است 🎮"
+    )
+
+print("FFASHBot Online")
+
+bot.run()
