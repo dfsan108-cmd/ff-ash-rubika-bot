5b0c56d77d813f72e643de72ec45bbbaf8267a83
dfsan108-cmd <dfsan108@gmail.com>
2026-07-21 22:07:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-import os
-import time
-from flask import Flask
+import rubika_bot
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "FFASHBot Online"
-
-def run_bot():
-    while True:
-        print("FFASHBot Online")
-        time.sleep(60)
-
-if __name__ == "__main__":
-    from threading import Thread
-
-    Thread(target=run_bot).start()
-
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+print(dir(rubika_bot))
