78d01ca4f4e900c1c4680d705bf98d24c6979a33
dfsan108-cmd <dfsan108@gmail.com>
2026-07-22 09:51:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
 
+players = []
+
 @app.route("/")
 def home():
-    return "FFASH Rubika Bot Online"
+    return "FFASH Bot Online"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    print("NEW UPDATE:", data)
+
+    players.append(data)
+
+    return {"status": "ok"}
+
+@app.route("/players")
+def show_players():
+    return {"count": len(players), "players": players}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
