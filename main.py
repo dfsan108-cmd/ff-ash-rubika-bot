9efbd926ff2c68a7e1b089ec83b64d993e715d3c
dfsan108-cmd <dfsan108@gmail.com>
2026-07-22 10:18:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 
 app = Flask(__name__)
 
+TOKEN = os.getenv("BOT_TOKEN")
+print("Token Loaded:", TOKEN is not None)
+
 players = []
 
 @app.route("/")
@@ -28,4 +31,7 @@ def get_players():
     }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
