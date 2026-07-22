3bf9ba6cd118b6e88704825c5a6f9c3ece61313e
dfsan108-cmd <dfsan108@gmail.com>
2026-07-22 10:23:21 +0330

    Create set_webhook.py

---
+++ set_webhook.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+url = f"https://botapi.rubika.ir/v3/{TOKEN}/updateBotEndpoint"
+
+data = {
+    "url": "https://ff-ash-rubika-bot-2.onrender.com/webhook"
+}
+
+response = requests.post(url, json=data)
+
+print(response.text)
