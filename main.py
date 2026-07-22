427be27aa913d78d4841337361e33607e4cf864d
dfsan108-cmd <dfsan108@gmail.com>
2026-07-22 09:26:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import os
-from flask import Flask
 import rubika_bot
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "FFASHBot Online"
-
-print(dir(rubika_bot))
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+print("VERSION:", getattr(rubika_bot, "__version__", "unknown"))
+print("DIR:", dir(rubika_bot))
